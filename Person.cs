542a2fdae80b4555a5b78e4ca4c509205118bde8
Arma Burton <arma@armaburton.com>
2023-05-30 13:29:29 -0700

    update

--- Person.cs
+++ Person.cs
@@ -1,6 +1,6 @@
 ﻿namespace Packt.shared;
 
-public class Person : Object{
+public class Person : object{
   //properties
   public string? Name { get; set; }
   public DateTime DateOfBirth { get; set; }
