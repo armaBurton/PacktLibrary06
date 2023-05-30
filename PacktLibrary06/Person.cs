663f68d21e988d0ee8e7d7c4643f3a2400c86c53
Arma Burton <arma@armaburton.com>
2023-05-30 12:53:34 -0700

    Person class

--- PacktLibrary06/Person.cs
+++ PacktLibrary06/Person.cs
@@ -1,5 +1,12 @@
-﻿namespace PacktLibrary06;
-public class Class1
-{
+﻿namespace Packt.Shared;
 
+public class Person : Object{
+  //properties
+  public string? Name { get; set; }
+  public DateTime DateOfBirth{ get; set; }
+
+  //methods
+  public void WriteToConsole(){
+    WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+  }
 }
