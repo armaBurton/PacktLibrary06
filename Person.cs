6b2eab4d67f1cc3caa0c690ad6e84b678b0a119f
Arma Burton <arma@armaburton.com>
2023-06-14 18:58:03 -0700

    {delegate}

--- Person.cs
+++ Person.cs
@@ -9,4 +9,22 @@ public class Person : object{
   public void WriteToConsole(){
     WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
   }
+
+  //delegate field
+  public EventHandler? Shout;
+
+  //data
+  public int AngerLevel;
+
+  //method
+  public void poke(){
+    AngerLevel++;
+    if (AngerLevel >= 3){
+      //if something is listening
+      if(Shout != null){
+        //...then call the delegate
+        Shout(this, EventArgs.Empty);
+      }
+    }
+  }
 }
