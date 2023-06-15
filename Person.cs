616d5e8bb2d1098e0ddf517e4705927bef2e1485
Arma Burton <arma@armaburton.com>
2023-06-15 13:40:24 -0700

    {comparing objects when sorting}

--- Person.cs
+++ Person.cs
@@ -1,6 +1,6 @@
 ﻿namespace Packt.Shared;
 
-public class Person : object{
+public class Person : object, IComparable<Person?>{
   //properties
   public string? Name { get; set; }
   public DateTime DateOfBirth { get; set; }
@@ -27,4 +27,29 @@ public class Person : object{
       }
     }
   }
+
+    public int CompareTo(Person? other){
+      int position;
+      
+      if((this is not null) && (other is not null)){
+        if((Name is not null) && (other.Name is not null)){
+          //if both Name values are not null
+          //use the string implementation of CompareTo
+          position = Name.CompareTo(other.Name);
+        } else if ((Name is not null) && (other.Name is null)){
+          position = -1;
+        } else if ((Name is null) && (other.Name is not null)){
+          position = 1;
+        } else {
+          position = 0;
+        }
+      } else if ((this is not null) && (other is null)){
+        position = -1;
+      } else if ((this is null) && (other is not null)){
+        position = 1;
+      } else {
+        position = 0;
+      }
+      return position;
+    }
 }
