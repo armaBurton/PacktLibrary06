bc13c02dda7e5ab5a6f30b5795295374f8f28b17
Arma Burton <arma@armaburton.com>
2023-06-16 10:54:15 -0700

    {delete gitignore cache}

--- Person.cs
+++ Person.cs
@@ -28,28 +28,28 @@ public class Person : object, IComparable<Person?>{
     }
   }
 
-    public int CompareTo(Person? other){
-      int position;
-      
-      if((this is not null) && (other is not null)){
-        if((Name is not null) && (other.Name is not null)){
-          //if both Name values are not null
-          //use the string implementation of CompareTo
-          position = Name.CompareTo(other.Name);
-        } else if ((Name is not null) && (other.Name is null)){
-          position = -1;
-        } else if ((Name is null) && (other.Name is not null)){
-          position = 1;
-        } else {
-          position = 0;
-        }
-      } else if ((this is not null) && (other is null)){
+  public int CompareTo(Person? other){
+    int position;
+    
+    if((this is not null) && (other is not null)){
+      if((Name is not null) && (other.Name is not null)){
+        //if both Name values are not null
+        //use the string implementation of CompareTo
+        position = Name.CompareTo(other.Name);
+      } else if ((Name is not null) && (other.Name is null)){
         position = -1;
-      } else if ((this is null) && (other is not null)){
+      } else if ((Name is null) && (other.Name is not null)){
         position = 1;
       } else {
         position = 0;
       }
-      return position;
+    } else if ((this is not null) && (other is null)){
+      position = -1;
+    } else if ((this is null) && (other is not null)){
+      position = 1;
+    } else {
+      position = 0;
     }
+    return position;
+  }
 }
