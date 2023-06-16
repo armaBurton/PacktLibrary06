fd1ec5926f2ea6a3d8492b74ce19818a2e0fdca3
Arma Burton <arma@armaburton.com>
2023-06-15 17:38:05 -0700

    {DvdPlayer.cs}

---
+++ DvdPlayer.cs
@@ -0,0 +1,11 @@
+namespace Packt.Shared;
+
+public class DvdPlayer{
+    public void Pause(){
+        WriteLine("DvD player is pausing.");
+    }
+
+    public void Play(){
+        WriteLine("DvD player is playing.");
+    }
+}
